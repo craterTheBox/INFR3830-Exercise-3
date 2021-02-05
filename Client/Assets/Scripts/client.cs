c03e2599bf5a69b983b5fc91c49c1b006cb2e577
Carter Menary <carter.menary@gmail.com>
2021-02-04 22:38:54 -0500

    Added both working Unity projects

---
+++ Client/Assets/Scripts/client.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Lec4
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+
+public class client : MonoBehaviour
+{
+    public GameObject myCube;
+
+    private static byte[] outBuffer = new byte[512];
+    private static IPEndPoint remoteEP;
+    private static Socket client_socket;    
+
+    public static void RunClient() {
+        IPAddress ip = IPAddress.Parse("10.0.0.50");
+        remoteEP = new IPEndPoint(ip, 11111);
+
+        client_socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        try {
+            outBuffer = Encoding.ASCII.GetBytes("Testing network......");
+            client_socket.SendTo(outBuffer, remoteEP);
+
+            //client_socket.Shutdown(SocketShutdown.Both);
+            //client_socket.Close();
+        }
+        catch (Exception e) {
+            Debug.LogException(e);
+        }
+    }
+
+    public static void CloseClient() {
+        client_socket.Shutdown(SocketShutdown.Both);
+        client_socket.Close();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCube = GameObject.Find("Cube");
+
+        RunClient();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Cube x: " + myCube.transform.position.x + "\ty: " + myCube.transform.position.y + "\tz: " + myCube.transform.position.z);
+
+        //string cubeTransform = "x: " + myCube.transform.position.x.ToString() + " y: " + myCube.transform.position.y.ToString() + " z: " + myCube.transform.position.z.ToString();
+        //outBuffer = Encoding.ASCII.GetBytes(cubeTransform);
+        //outBuffer = BitConverter.GetBytes(myCube.transform.position.x);
+
+        outBuffer = Encoding.ASCII.GetBytes(myCube.transform.position.x.ToString());
+
+        client_socket.SendTo(outBuffer, remoteEP);
+
+    }
+}
---
+++ Server/Assets/Scripts/server.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+public class server : MonoBehaviour
+{
+
+    public static GameObject yourCube;
+    private byte[] buffer;
+    private IPHostEntry host;
+    private IPAddress ip = IPAddress.Parse("10.0.0.50");
+    private IPEndPoint localEP;
+    private Socket serverSocket;
+    private IPEndPoint client;
+    private EndPoint remoteClient;
+
+    void RunServer()
+    {
+        buffer = new byte[512];
+
+        host = Dns.GetHostEntry(Dns.GetHostName());
+
+        localEP = new IPEndPoint(ip, 11111);
+
+        serverSocket = new Socket(ip.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
+
+        client = new IPEndPoint(IPAddress.Any, 0); //0 for any available port
+        remoteClient = (EndPoint)client;
+
+        serverSocket.Bind(localEP);
+
+        //Console.WriteLine("Waiting for data...");
+        //Console.Read();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        yourCube = GameObject.Find("Cube");
+        RunServer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int rec = serverSocket.ReceiveFrom(buffer, ref remoteClient);
+
+        //Console.WriteLine("Received: {0}    from Client: {1}", Encoding.ASCII.GetString(buffer, 0, rec), remoteClient.ToString());
+
+        float networkX = float.Parse(Encoding.ASCII.GetString(buffer, 0, rec));
+        Vector3 networkTransform = new Vector3(networkX, 1.0f, 0.0f);
+        yourCube.transform.position = networkTransform;
+        Debug.Log("Updated Cube's x to: " + yourCube.transform.position.x);
+    }
+}
